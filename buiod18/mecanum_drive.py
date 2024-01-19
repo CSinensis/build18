74f87b8aae50cbfb6a334712519bdc1d77816fa4
Melinda Chen <melinda.chen16@gmail.com>
2024-01-18 23:58:13 -0500

    AA

--- buiod18/mecanum_drive.py
+++ buiod18/mecanum_drive.py
@@ -1,43 +1,51 @@
 import numpy as np
 import busio
 import board
-from adafruit_motorkit import MotorKit
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import motor
 
 class MecanumDriver:
     def __init__(self,
-                 pwm_frequency=1600,
+                 pca_frequency=1600,
                  whl_radius=0.02,
                  max_whl_speed=20,
                  body_length=0.3,
                  wheel_base=0.2):
         i2c_bus = busio.I2C(board.SCL,board.SDA)
-        self.kit = MotorKit(address=0x60, i2c=i2c_bus, pwm_frequency=pwm_frequency)
+        pca = PCA9685(i2c_bus, address=0x60)
+        pca.frequency = pca_frequency
+
+        self.motor1 = motor.DCMotor(pca.channels[9],pca.channels[10])
+        self.motor2 = motor.DCMotor(pca.channels[11],pca.channels[12])
+        self.motor3 = motor.DCMotor(pca.channels[3],pca.channels[4])
+        self.motor4 = motor.DCMotor(pca.channels[5],pca.channels[6])
+
         self.whl_radius = whl_radius
         self.max_whl_speed = max_whl_speed
         self.body_length = body_length
         self.wheel_base = wheel_base
     
     def test_spin(self,throttle_val):
-        self.kit.motor1.throttle = throttle_val
-        self.kit.motor2.throttle = throttle_val
-        self.kit.motor3.throttle = throttle_val
-        self.kit.motor4.throttle = throttle_val
+        self.motor1.throttle = throttle_val
+        self.motor2.throttle = throttle_val
+        self.motor3.throttle = throttle_val
+        self.motor4.throttle = throttle_val
 
 
     def send_action(self, action):
         throttles = self._whl_speeds_to_throttle(self._action_to_whl_speeds(action))
 
         # remember to flip 2 and 4
-        self.kit.motor1.throttle = throttles[0]
-        self.kit.motor2.throttle = throttles[1]
-        self.kit.motor3.throttle = throttles[2]
-        self.kit.motor4.throttle = throttles[3]
+        self.motor1.throttle = throttles[0]
+        self.motor2.throttle = throttles[1]
+        self.motor3.throttle = throttles[2]
+        self.motor4.throttle = throttles[3]
     
     def zero_all(self):
-        self.kit.motor1.throttle = 0
-        self.kit.motor2.throttle = 0
-        self.kit.motor3.throttle = 0
-        self.kit.motor4.throttle = 0
+        self.motor1.throttle = 0
+        self.motor2.throttle = 0
+        self.motor3.throttle = 0
+        self.motor4.throttle = 0
 
     def _action_to_whl_speeds(self, action):
         mag = np.linalg.norm(action[:2]) / self.whl_radius
